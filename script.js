8d5035a4d37edeceb4e4f1b6bf5ae1f763b3827a
franticnoise <z1priano@gmail.com>
2022-08-16 09:50:07 -0600

    1 binaries uploaded

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var btn_1 = document.querySelector('.dropdown_desktop_icon button');
 var btn_2 = document.querySelector('.dropdown_desktop button');
 var btn_3 = document.querySelector('.dropdown_mobile button');
@@ -15,10 +13,6 @@ btn_2.addEventListener('click', function () {
 btn_3.addEventListener('click', function () {
   this.classList.toggle('active');
 });
-
-/**
-code for closing the dropdown when we click ouside the dropdown
-**/
 
 window.addEventListener('click', function (event) {
   if (
@@ -37,5 +31,3 @@ window.addEventListener('click', function (event) {
     });
   }
 });
-
-
