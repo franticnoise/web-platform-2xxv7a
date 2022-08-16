97d8e84d9880e128a29244e5ff1b8c1e9689d859
franticnoise <z1priano@gmail.com>
2022-08-16 09:20:45 -0600

    4 files modified

--- menu.js
+++ menu.js
@@ -5,36 +5,10 @@ document.getElementById('myNav').innerHTML =
   '    <span class=text>MUSIC</span>' +
   '  </button>' +
   '  <ul>' +
-  '    <li>' +
-  '      <a href=>' +
-  "        <span class='icon'>" +
-  "          <ion-icon name='person-sharp'></ion-icon>" +
-  '        </span>' +
-  "        <span class='text'>Releases</span>" +
-  '      </a>' +
-  '    </li>' +
-  '    <li>' +
-  '      <a href=>' +
-  "        <span class='icon'>" +
-  "          <ion-icon name='key-sharp'></ion-icon>" +
-  '        </span>' +
-  "        <span class='text'>Soundcloud</span>" +
-  '      </a>' +
-  '    </li>' +
-  '    <li>' +
-  "      <a href='#''>" +
-  "        <span class='icon'><ion-icon name='bookmark-sharp'></ion-icon></span>" +
-  "        <span class='text'>Youtube</span>" +
-  '      </a>' +
-  '   </li>' +
-  '   <li>' +
-  '     <a href=>' +
-  "       <span class='icon'>" +
-  "         <ion-icon name='bookmark-sharp'></ion-icon>" +
-  '       </span>' +
-  "       <span class='text'>BandCamp</span>" +
-  '      </a>' +
-  '   </li>' +
+  '    <li><a href=https://web-platform-2xxv7a.stackblitz.io/index.html><span class=text>Releases</span></a></li>' +
+  '    <li><a href=https://web-platform-2xxv7a.stackblitz.io/Soundcloud.html><span class=text>Soundcloud</span></a></li>' +
+  '    <li><a href=https://web-platform-2xxv7a.stackblitz.io/Videos.html><span class=text>Videos</span></a></li>' +
+  '   <li><a href=https://web-platform-2xxv7a.stackblitz.io/Bandcamp.html><span class=text>BandCamp</span></a></li>' +
   ' </ul>' +
   '</div>' +
   "<div class='dropdown dropdown_desktop_icon'>" +
@@ -42,19 +16,11 @@ document.getElementById('myNav').innerHTML =
   "    <span class='text'>Presets</span>" +
   '  </button>' +
   '  <ul>' +
-  '    <li>' +
-  "      <a href='https://web-platform-2xxv7a.stackblitz.io/VSTpresets.html'>" +
-  "        <span class='text'>VST Presets</span>" +
-  '      </a>' +
-  '    </li>' +
-  '    <li>' +
-  "      <a href='#'><span class='text'>Soundbanks</span></a>" +
-  '    </li>' +
-  '    <li>' +
-  "      <a href='https://web-platform-2xxv7a.stackblitz.io/Templates.html'><span class='text'>Templates</span></a>" +
-  '    </li>' +
-  "    <li><a href=><span class='text'>Full Courses</span></a></li>" +
-  "    <li><a href=><span class='text'>Webinars</span></a></li>" +
+  '    <li><a href=https://web-platform-2xxv7a.stackblitz.io/VSTpresets.html><span class=text>VST Presets</span></a></li>' +
+  '    <li><a href=https://web-platform-2xxv7a.stackblitz.io/Libraries.html><span class=text>Soundbanks</span></a></li>' +
+  '    <li><a href=https://web-platform-2xxv7a.stackblitz.io/Templates.html><span class=text>Templates</span></a></li>' +
+  "    <li><a href=https://web-platform-2xxv7a.stackblitz.io/FullCourses.html><span class='text'>Courses Webinars</span></a></li>" +
+  "    <li><a href=https://web-platform-2xxv7a.stackblitz.io/Memberships.html><span class='text'>Memberships</span></a></li>" +
   '  </ul>' +
   '</div>' +
   "<div class='dropdown dropdown_mobile'>" +
@@ -62,26 +28,10 @@ document.getElementById('myNav').innerHTML =
   "    <span class='text'>SOCIALMEDIA</span>" +
   '  </button>' +
   '  <ul>' +
-  '    <li>' +
-  "      <a href=><span class='text'>Instagram</span></a>" +
-  '    </li>' +
-  '    <li>' +
-  "      <a href=><span class='text>Youtube</span></a>" +
-  '    </li>' +
-  '    <li>' +
-  '      <a href=>' +
-  "        <span class='icon'><ion-icon name='ion-android-cloud-circle'></ion-icon></span>" +
-  "       <span class='text'>Soundcloud</span>" +
-  '      </a>' +
-  '    </li>' +
-  '    <li>' +
-  '      <a href=>' +
-  "        <span class='icon'><ion-icon name='bookmark-sharp'></ion-icon></span>" +
-  "        <span class='text'>Facebook</span>" +
-  '      </a>' +
-  '    </li>' +
-  '    <li>' +
-  '      <a href=><span class=text>Twitch</span></a>' +
-  '    </li>' +
+  '    <li><a href=http://www.instagram.com/franticnoise target=_blank><span class=text>Instagram</span></a></li>' +
+  '    <li><a href=http://www.youtube.com/hacerruido target=_blank><span class=text>Youtube</span></a></li>' +
+  '    <li><a href=http://www.soundcloud.com/franticnoise target=_blank><span class=text>Soundcloud</span></a></li>' +
+  '    <li><a href=http://www.facebook.com/franticnoise target=_blank><span class=text>Facebook</span></a></li>' +
+  '    <li><a href=http://www.twitch.com/franticnoise target=_blank><span class=text>Twitch</span></a></li>' +
   '  </ul>' +
   '</div>';
